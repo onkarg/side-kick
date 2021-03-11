189e78f05d154fd89ce1f0e6e54c23e3486602b4
onkarg <onkarg@gmail.com>
2021-03-11 10:50:52 -0800

    project

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import icon from '../assets/icon.svg';
+import './App.global.css';
+
+const Hello = () => {
+  const [latestCommit, setLatestCommit] = useState(null);
+
+  const getLatestCommit = () => {
+    fetch('http://localhost:3001/')
+      .then((res) => res.json())
+      .then((data) => console.log('data', data))
+      .catch((err) => console.log('err', err));
+  };
+
+  useEffect(() => {
+    getLatestCommit();
+  }, []);
+  return (
+    <div>
+      <div className="Hello">
+        <img width="200px" alt="icon" src={icon} />
+      </div>
+      <h1>electron-react-boilerplate</h1>
+      <div className="Hello">
+        <a
+          href="https://electron-react-boilerplate.js.org/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="books">
+              📚
+            </span>
+            Login with Github
+          </button>
+        </a>
+        <a
+          href="https://github.com/sponsors/electron-react-boilerplate"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="books">
+              🙏
+            </span>
+            Donate
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Hello} />
+      </Switch>
+    </Router>
+  );
+}
